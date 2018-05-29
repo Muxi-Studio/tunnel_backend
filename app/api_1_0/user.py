a546e0678e23a287976663a427c8636ae4ed4320
Darren2017 <17362990052@163.com>
2018-05-29 20:57:49 +0800

    fix sent_time切片

--- app/api_1_0/user.py
+++ app/api_1_0/user.py
@@ -8,15 +8,17 @@ from app import db
 def message():
     sent_content = request.get_json().get('sent_content')
     sent_name = request.get_json().get('sent_name')
-    sent_time = request.get_json().get('sent_time')
+    sent_time_try = request.get_json().get('sent_time')
     sent_address = request.get_json().get('sent_address')
 
+    sent_time = sent_time_try[0:10]
+
     message = Message(content=sent_content,
-                      way=2,
-                      name=sent_name,
-                      time=sent_time,
-                      address=sent_address,
-                      status=1)
+                    way=2,
+                    name=sent_name,
+                    time=sent_time,
+                    address=sent_address,
+                    status=1)
     try:
         db.session.add(message)
         db.session.commit()
